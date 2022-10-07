d16b86f8a318852a70a04ccc9eecea0073c1551b
Neben <neben@macben.local>
2022-10-07 19:12:00 +0200

    colleen

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,18 +1,11 @@
-/*
-	Outside comment
-*/
-
+/*Outside comment*/
 #include <stdio.h>
-
-void another_function()
-{
-	printf("/*\n	Outside comment\n*/\n\n#include<stdio.h>\n\nvoid another_function()\n{\n	printf(\"/*\n	Outside comment\n")
+void another_function() {
+char *c="/*Outside comment*/%c#include <stdio.h>%cvoid another_function() {%cchar *c=%c%s%c;%cprintf(c,10,10,10,34,c,34,10,10,10,10,10,10,10,10);%c}%cint main() {%c/*Inside comment*/%canother_function();%creturn 0;%c}%c";
+printf(c,10,10,10,34,c,34,10,10,10,10,10,10,10,10);
 }
-
-int main()
-{
-	/*
-		Inside comment
-	*/
-	return 0;
+int main() {
+/*Inside comment*/
+another_function();
+return 0;
 }
