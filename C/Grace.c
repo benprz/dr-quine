d388e57624c0cfadc3246c9f43b60a0b59f4ae02
Neben <neben@macben.local>
2022-10-08 15:14:37 +0200

    edit undeclared main (as subject asks)

--- C/Grace.c
+++ C/Grace.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 #define foo main
-#define X "/*comment*/%c#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#include <string.h>%c#define foo main%c#define X %c%s%c%c#define F int foo() {%c%c%cint fd;%c%c%cif ((fd = open(%cGrace_kid.c%c, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR)) != -1) {%c%c%c%cchar s[1000];%c%c%c%cint len = sprintf(s,X,10,10,10,10,10,10,34,X,34,10,92,10,9,92,10,9,34,34,92,10,9,9,92,10,9,9,92,10,9,9,92,10,9,9,92,10,9,92,10,9,92,10,10,10,10);%c%c%c%cs[len] = 0;%c%c%c%cwrite(fd, s, len);%c%c%c}%c%c%creturn 0;%c%c}%c%cF%c"
-#define F int foo() {\
+#define X "/*comment*/%c#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#include <string.h>%c#define foo main%c#define X %c%s%c%c#define FT(x)int foo() {%c%c%cint fd;%c%c%cif ((fd = open(%cGrace_kid.c%c, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR)) != -1) {%c%c%c%cchar s[1000];%c%c%c%cint len = sprintf(s,X,10,10,10,10,10,10,34,X,34,10,92,10,9,92,10,9,34,34,92,10,9,9,92,10,9,9,92,10,9,9,92,10,9,9,92,10,9,92,10,9,92,10,10,10,10);%c%c%c%cs[len] = 0;%c%c%c%cwrite(fd, s, len);%c%c%c}%c%c%creturn 0;%c%c}%c%cFT(xxxxxxxx)%c"
+#define FT(x)int foo() {\
 	int fd;\
 	if ((fd = open("Grace_kid.c", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR)) != -1) {\
 		char s[1000];\
@@ -16,4 +16,4 @@
 	return 0;\
 }
 
-F
+FT(xxxxxxxx)
